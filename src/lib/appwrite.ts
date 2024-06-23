5f9f184d2418305d8d703836f72a8cfb82ccd443
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-23 21:14:16 +0700

    resetup the project

--- src/lib/appwrite.ts
+++
@@ -1,8 +0,0 @@
-import { Client, Account } from "appwrite";
-const appwriteId = import.meta.env.VITE_APPWRITE_PROJECT_ID;
-
-export const client = new Client();
-client.setEndpoint("https://cloud.appwrite.io/v1").setProject(appwriteId);
-
-export const account = new Account(client);
-export { ID } from "appwrite";
