05fceca4737622e58960c26b03dfcb616efa878f
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-23 23:16:30 +0700

    resetup project with vue

--- frontend/utils/appwrite.js
+++
@@ -1,8 +0,0 @@
-import { Account, Client } from "appwrite";
-
-export const useAppwriteService = (runtimeConig) => {
-	const { AppwriteEndpoint, AppwriteProjectId } = runtimeConig;
-
-	const client = new Client();
-	client.setEndpoint(AppwriteEndpoint).setProject(AppwriteProjectId);
-};
