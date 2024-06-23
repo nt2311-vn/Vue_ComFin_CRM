4dd06fb986c74c921df86fea7cfa41157c222bfe
nt2311-vn <nguyentoan231196@gmail.com>
2024-06-24 00:56:06 +0700

    test page

--- frontend/src/components/Navbar.svelte
+++ frontend/src/components/Navbar.svelte
@@ -1,18 +1,17 @@
 <script>
+	import { onMount } from 'svelte';
 	let currentTheme = 'light';
 	const themes = [
 		'light',
 		'dark',
 		'emerald',
 		'corporate',
-		'cyberpunk',
 		'valentine',
 		'halloween',
 		'forest',
 		'lofi',
 		'pastel',
 		'fantasy',
-		'wireframe',
 		'black',
 		'dracula',
 		'cmyk',
@@ -26,5 +25,33 @@
 
 	const chooseTheme = (theme) => {
 		currentTheme = theme;
+		document.documentElement.setAttribute('data-theme', theme);
 	};
+
+	onMount(() => {
+		document.documentElement.setAttribute('data-theme', currentTheme);
+	});
 </script>
+
+<nav class="navbar bg-primary/50 backdrop-blur-md text-primary-content">
+	<div class="navbar-start">
+		<a href="/" class="btn btn-ghost normal-case text-xl">App Logo</a>
+	</div>
+	<div class="navbar-end flex items-center space-x-4">
+		<div class="dropdown">
+			<button tabindex="0" class="btn btn-ghost btn-circle dropdown-toggle">
+				<span>Select Theme: {currentTheme}</span>
+			</button>
+			<ul
+				tabindex="0"
+				class="menu menu-compact dropdown-content mt-3 p-2 shadow-lg bg-base-100/80 backdrop-blur-md rounded-box w-52"
+			>
+				<li class="menu-title"><span>Themes</span></li>
+				{#each themes as theme}
+					<li><a href="#" role="button" on:click={() => chooseTheme(theme)}>{theme}</a></li>
+				{/each}
+			</ul>
+		</div>
+		<button class="btn btn-outline btn-primary ml-2" on:click={() => {}}>Logout</button>
+	</div>
+</nav>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Navbar from '../components/Navbar.svelte';
+	import '../app.css';
+</script>
+
+<div>
+	<Navbar />
+	<h1>Welcome to SvelteKit</h1>
+	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
